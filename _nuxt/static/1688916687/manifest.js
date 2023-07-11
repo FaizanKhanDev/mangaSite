9c38fbe26050767b837f3f98981ee613ff9cc614
FaizanKhanDev <117845718+FaizanKhanDev@users.noreply.github.com>
2023-07-11 05:41:39 +0000

    Deploying to gh-pages from @ FaizanKhanDev/mangaSite@49a16485b2c696f4dcdce1a38644d33b62763c5b 🚀

--- _nuxt/static/1688916687/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcomics\u002Fmanga","\u002Fposts\u002Fbookmarks","\u002Fcomics\u002Fmanhua","\u002Fcomics\u002Fmanhwa","\u002Fposts\u002Ftrendings"]})
